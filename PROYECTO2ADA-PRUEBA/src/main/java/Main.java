a1461e36c644d8243724f7b43e7b14cadce68205
manuel-ir <minfarodriguez@gmail.com>
2026-06-04 03:03:59 +0200

    funcionalidad login

--- PROYECTO2ADA-PRUEBA/src/main/java/Main.java
+++ PROYECTO2ADA-PRUEBA/src/main/java/Main.java
@@ -1,6 +1,14 @@
+import controlador.ControladorLogin;
+import modelo.GestorBD;
+import vista.VistaLogin;
+
 public class Main {
 
     public static void main(String[] args) {
-        // pendiente
+        // arranca la aplicación con la vista de login
+        GestorBD modelo = new GestorBD();
+        VistaLogin vista = new VistaLogin();
+        new ControladorLogin(vista, modelo);
+        vista.setVisible(true);
     }
 }
